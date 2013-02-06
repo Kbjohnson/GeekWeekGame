640c23285215783dd20acbbf6ee850156312fbd6
Kyle Johnson <user@ubuntu.(none)>
2013-02-06 10:43:40 -0800

    initial commit

---
+++ gameLogic.js
@@ -0,0 +1,14 @@
+// Context Variables
+var ctxBG = document.getElementById('canvasBg').getContext('2d');
+var ctxGeek = document.getElementById('geek').getContext('2d');  
+var ctxSquirrel = document.getElementById('obstacle').getContext('2d');
+var ctxScore = document.getElementById('score').getContext('2d'); 
+var ctxLives = document.getElementById('lives').getContext('2d'); 
+
+//HUD 
+ctxScore = ;  
+ctxLives = ; 
+
+//Global Variables
+
+var squirrel
